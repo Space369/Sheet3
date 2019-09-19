faa21ba8d41bc1c34dd3c93ae2afa66f9d05e3ce
Space369 <32286421+Space369@users.noreply.github.com>
2019-09-19 18:49:08 -0400

    Add project files.

---
+++ Sheet3/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sheet3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            DBManager db = new DBManager();
+            var records = db.GetAll();
+
+            return View(records);
+
+
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(ContactRec c)
+        {
+            DBManager db = new DBManager();
+            db.Add(c);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            DBManager db = new DBManager();
+            var currRecord = db.Details(id);
+            return View(currRecord);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            DBManager db = new DBManager();
+            db.Delete(id);
+
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            DBManager db = new DBManager();
+            var record = db.Details(id);
+
+            return View(record);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ContactRec c)
+        {
+            DBManager db = new DBManager();
+            db.Update(c);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
